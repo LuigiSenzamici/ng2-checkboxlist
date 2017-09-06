8771a6ab39cc6d405b9a76a63f7e5f786fede571
Luigi Senzamici <luigisenzamici@gmail.com>
2017-09-06 23:05:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Check Box List
+Angular 2 checkbox list component
+## Getting Started
+```bash
+npm install ng2-checkboxlist --save
+```
+## Simple Use
+
+## Styling
+
+## Documentation
+
+## Built With
+Typescript
+
+Stylus
+## Author
+
+[Luigi Senzamici](http://luigisenzamici.com)
+
+
+## License
+
+This project is licensed under the MIT License 
+
+
+
---
+++ checkboxlist.component.ts
@@ -0,0 +1,108 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { NgClass } from '@angular/common';
+@Component({
+    selector: 'checkbox-list',
+    template: `
+    <div [ngClass]="containerClasses">
+        <div [ngClass]="titleClasses">{{title}}</div>
+        <div *ngFor="let e of list">
+        <input type="checkbox" [ngClass]="inputClasses" name="checkb" id="checkb-{{e[id]}}" value="{{e[id]}}" (click)="clickManage(e[id])" />
+                                    <label [ngClass]="labelClasses"  for="checkb-{{e[id]}}">{{e[value]}}</label>
+        </div>
+    </div>
+    `,
+    styles:[`
+        .container-struct{
+            display: inline-block;
+            padding: 10px;
+        }
+        .title-struct{
+            text-align:center;
+            font-weight: 700;
+        }
+        .container-base{
+            border: 1px solid gray;
+            border-radius: 5px;
+            font-family:'Arial'
+        }
+    `]
+})
+export class CheckBoxList{
+    @Input() list:any[];
+    @Input() title:string = '';
+    @Input() id:string= '';
+    @Input() value:string= '';
+    @Input() set styles(values){
+        if(typeof values === 'string'){//setting "container:redClass, shadow";
+            let r = this.readSetting(values);
+            this.setClass(r.compo, r.values);
+        }
+        if(Array.isArray(values)){//setting ["container:greenClass, shadow", "title:whiteClass"]
+            values.forEach(e=>{
+                let r = this.readSetting(e);
+                this.setClass(r.compo, r.values);
+            });
+        }
+    }
+    _listSelected:any[] = [];
+    @Output() listSelected:EventEmitter<any[]> = new EventEmitter<any[]>();
+    containerClasses:string[] = ['container-struct', 'container-base'];
+    titleClasses:string[] = ['title-struct'];
+    inputClasses:string[] =[];
+    labelClasses:string[] =[];
+    private readSetting(value:string):any{
+        let c:string[] = value.split(":");
+        let v:string[] = c[1].split(",")
+        let res = {
+            compo:c[0],
+            values:v
+        }
+        return res;
+    }
+    private setClass(compo:string, values:string[]){
+        switch(compo){
+            case 'container': values.forEach(e=>this.containerClasses.push(e.trim()));break;
+            case 'title': values.forEach(e=>this.titleClasses.push(e.trim()));break;
+            case 'input': values.forEach(e=>this.inputClasses.push(e.trim()));break;
+            case 'label': values.forEach(e=>this.labelClasses.push(e.trim()));break;
+        }
+    }
+    private removeClass(compo:string, value:string){
+        switch(compo){
+            case 'container': this.containerClasses.splice(this.containerClasses.indexOf(value), 1);break;
+            case 'title': this.titleClasses.splice(this.titleClasses.indexOf(value), 1);break;
+            case 'input': this.inputClasses.splice(this.inputClasses.indexOf(value), 1);break;
+            case 'label': this.labelClasses.splice(this.labelClasses.indexOf(value), 1);break;    
+        }
+    }
+    private clickManage(id:string){
+        let ids = "checkb-" + id;
+        let nodo = (document.getElementById(ids)) as HTMLInputElement;
+         if(nodo){
+            let sel = nodo.checked;
+            if(sel){
+                let res ={
+                    tagId: ids,
+                    value: id,
+                    text: this.list.filter(e=>e.id == id)[0].text
+                }
+                this._listSelected.push(res);
+                this.listSelected.emit(this._listSelected);
+                return;
+            }
+            this._listSelected.every(
+                (e, i, a): boolean =>{
+                    if(e.value == id){
+                        this._listSelected.splice(i, 1);
+                        return false;
+                    }
+                    return true;
+                }
+            );
+            this.listSelected.emit(this._listSelected);            
+        }
+        
+    }
+
+    
+}
